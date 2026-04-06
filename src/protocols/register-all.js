3657d729743d553ba2a1833da7aa87485f9c1841
Jorrel Patterson <jorrelpatterson@users.noreply.github.com>
2026-04-06 12:44:01 -0700

    feat: register money protocols (credit, income) with pipeline

--- src/protocols/register-all.js
+++ src/protocols/register-all.js
@@ -2,7 +2,12 @@ import { registerProtocol } from './registry';
 import workoutProtocol from './body/workout/index';
 import peptideProtocol from './body/peptides/index';
 import nutritionProtocol from './body/nutrition/index';
+import creditProtocol from './money/credit/index';
+import incomeProtocol from './money/income/index';
 
 registerProtocol(workoutProtocol);
 registerProtocol(peptideProtocol);
 registerProtocol(nutritionProtocol);
+registerProtocol(creditProtocol);
+registerProtocol(incomeProtocol);
+
